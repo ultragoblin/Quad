4f5ed99b596b8f3d282d7dfc293af41582082eb9
ultragoblin <angry.person1@gmail.com>
2016-06-07 14:47:50 +0300

    Добавил настройку по клавише "c"

--- Tools/copter_motor_test/copter_motor_test.ino
+++ Tools/copter_motor_test/copter_motor_test.ino
@@ -1,10 +1,11 @@
 
-
+String str;
 int ch;
 int cur_val=160;
 int max = 160;
 int min = 88;
 void setup() {
+  Serial.setTimeout(10000);
   Serial.begin(9600);
   pinMode(11, OUTPUT);
 }
@@ -14,17 +15,35 @@ void loop() {
     // get incoming byte:
     ch = Serial.read();
     
-    if (ch == 45 && cur_val > min){
+    if (ch == 45 && cur_val > min){  // "-" dec throttle
       cur_val--;
       Serial.println(String(cur_val));
     }
-    else if (ch == 43 && cur_val < max){
+    else if (ch == 43 && cur_val < max){  // "+" inc throttle
       cur_val++;
       Serial.println(String(cur_val));
     }
-    else if (ch == 32){
+    else if (ch == 32){  // "Space" zero throttle
       cur_val = min;
       Serial.println(String(cur_val));
+    }
+    else if (ch == 99){  // "c" configure
+      analogWrite(11,0);
+
+      Serial.println("Input min val:");
+      str = Serial.readStringUntil(13);
+      min = str.toInt();
+      Serial.println(min);
+      
+      Serial.println("Input max val:");
+      str = Serial.readStringUntil(13);
+      max = str.toInt();
+      Serial.println(max);
+
+      Serial.println("Input init val:");
+      str = Serial.readStringUntil(13);
+      cur_val = str.toInt();
+      Serial.println(cur_val);
     }
   }
   analogWrite(11,cur_val);
